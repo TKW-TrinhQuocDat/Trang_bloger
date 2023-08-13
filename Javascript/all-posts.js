920da58865606bd13427f21a5664f4fbf8f029aa
anhhao247 <anhhao9a9@gmail.com>
2023-08-13 09:29:54 +0700

    update code for feature-test

--- Javascript/all-posts.js
+++ Javascript/all-posts.js
@@ -1,31 +1,32 @@
-var cnt = 0;
-function onClick() {
-    
-    if(cnt === 0) {
-        document.querySelector('#ext').style.display = "flex"
-        cnt++;
-    }
-    else {
-        document.querySelector('#ext').style.display = "none"
-        cnt--;
-    }
+function createPost() {
+    fetch('../Json_Business/business-test.json')
+        .then((res) => res.json())
+        .then((data) => {
+            let output = ''
+            data.forEach((post) => {
+                output += `
+                    <div id=${post.id} class="post">
+                    <div class="post-img">
+                        <a href=${post.link}><img src=${post.img} alt=""></a>
+                    </div>
+                    <div class="post-content">
+                        <div class="post-tag"><a href="${post.link}">Business</a></div>
+                        <h2 class="post-title"><a href="${post.link}">${post.title}</a></h2>
+                        <div class="post-desc">
+                            ${post.discription}
+                         </div>
+                         <div class="subcontent">
+                            <div><i class="fa-solid fa-heart"></i>0</div>
+                            <div><i class="fa-solid fa-comment-dots"></i>0</div>
+                             <div><i class="fa-solid fa-share-nodes"></i></div>
+                         </div>
+                    </div>
+                </div>
+            `
+            })
+            document.getElementById("posts").innerHTML = output
+        })
 }
 
-var like_cnt = 0;
-function like() {
-    like_cnt++;
-    document.querySelector('#like-cnt').innerText = like_cnt;
-}
+createPost()
 
-var cnt = 0;
-function onClickYear() {
-    
-    if(cnt === 0) {
-        document.querySelector('#posts_2023').style.display = "none"
-        cnt++;
-    }
-    else {
-        document.querySelector('#posts_2023').style.display = "flex"
-        cnt--;
-    }
-}
